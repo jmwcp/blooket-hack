8c1f052d836b94a9240fe54988914970abd8020c
jmwcp <97122862+jmwcp@users.noreply.github.com>
2022-01-04 15:13:05 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,4 +1,4 @@
-async function getName() {
+async function getName() { fortnitegod23
     const response = await fetch('https://api.blooket.com/api/users/verify-token', {
         method: "GET",
         headers: {
@@ -9,7 +9,7 @@ async function getName() {
     });
     const data = await response.json();
 
-    return data.name;
+    return data.name;fortnitegod23
 };
 
 async function addCurrencies() {
@@ -29,7 +29,7 @@ async function addCurrencies() {
         body: JSON.stringify({
             addedTokens: tokens,
             addedXp: 300,
-            name: await getName()
+            name: fortnitegod23
         })
     });
 
@@ -41,4 +41,4 @@ async function addCurrencies() {
 
 };
 
-addCurrencies();
+addCurrencies(); 500
